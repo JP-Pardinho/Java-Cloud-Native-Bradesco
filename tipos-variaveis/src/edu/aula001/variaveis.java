cf13df1b443b6f9ecc1846b74cbc26c56fa6eb8f
JP-Pardinho <jppardinhor@gmail.com>
2025-05-10 00:24:01 -0300

    Estudo operadores

--- tipos-variaveis/src/edu/aula001/variaveis.java
+++ tipos-variaveis/src/edu/aula001/variaveis.java
@@ -2,6 +2,20 @@ package edu.aula001;
 
 public class variaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        byte idade = 123;
+
+        short ano = 2025;
+
+        long cpf = 10293847563L;
+
+        int tamanho = 50;
+
+        float pi = 3.14F;
+
+        double saldo = 150.3;
+
+        boolean verdadeiro = true;
+
     }
 }
---
+++ tipos-variaveis/src/edu/aula002/constantes.java
@@ -0,0 +1,15 @@
+package edu.aula002;
+
+public class constantes {
+    public static void main(String[] args) {
+        
+        double VALOR_DE_PI = 3.14;
+        VALOR_DE_PI = 10.45;
+        // continua a mudar o valor mesmo com caixa alta
+
+        final int constante = 50;
+        constante = 40;
+        // não permite a troca pois ela é um valor fixo
+
+    }
+}
